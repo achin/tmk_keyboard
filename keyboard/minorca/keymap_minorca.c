43df64a519320ac873f500e2b9230a40f9824750
Alex Chin <alex.ba.chin@gmail.com>
2016-09-15 08:16:21 -0400

    Line things up.

--- keyboard/minorca/keymap_minorca.c
+++ keyboard/minorca/keymap_minorca.c
@@ -5,7 +5,7 @@
      KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, \
-            TRNS, TRNS, TRNS,         TRNS,         TRNS,         TRNS, TRNS, TRNS),
+            TRNS, TRNS, TRNS,       TRNS,       TRNS,             TRNS, TRNS, TRNS),
 */
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -13,25 +13,25 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KEYMAP(TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC, \
             LCTL, A,    S,    D,    F,    G,    H,    J,    K,    L,    FN1, \
             LSFT, Z,    X,    C,    V,    B,          N,    M,    COMM, DOT,  SLSH, \
-            FN0,  LALT, LGUI,         ENT,          SPC,          FN2,  QUOT, RSFT),
+            FN0,  LALT, LGUI,       ENT,        SPC,              FN2,  QUOT, RSFT),
 
     /* 1: FN0 */
      KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LEFT, DOWN, UP,   RIGHT,TRNS, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, \
-            FN0 , TRNS, TRNS,         TRNS,         TRNS,         TRNS, TRNS, TRNS),
+            FN0 , TRNS, TRNS,       TRNS,       TRNS,             TRNS, TRNS, TRNS),
 
     /* 2: FN1 */
      KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, FN1,  \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, \
-            TRNS, TRNS, TRNS,         TRNS,         TRNS,         TRNS, TRNS, TRNS),
+            TRNS, TRNS, TRNS,       TRNS,       TRNS,             TRNS, TRNS, TRNS),
 
     /* 3: FN2 */
      KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, BTLD, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, \
-            TRNS, TRNS, TRNS,         TRNS,         TRNS,         FN2,  TRNS, TRNS),
+            TRNS, TRNS, TRNS,       TRNS,       TRNS,             FN2,  TRNS, TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
